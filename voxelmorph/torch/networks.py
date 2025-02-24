93c9b967772cadb3be7ad39d847391ed282fd3ba
Etienne Chollet <etiennepchollet@gmail.com>
2025-02-24 13:34:46 -0500

    [STYLE] Remove unused torch.nn.functional

--- voxelmorph/torch/networks.py
+++ voxelmorph/torch/networks.py
@@ -7,7 +7,6 @@ __all__ = [
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.distributions.normal import Normal
 
 from .. import default_unet_features
