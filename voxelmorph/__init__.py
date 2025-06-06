2c2041dc7498a637e05e4873b01ca8020fd8f769
Etienne Chollet <etiennepchollet@gmail.com>
2025-06-06 12:09:19 -0400

    [BREAKING] Remove tensorflow imports and backend checking

--- voxelmorph/__init__.py
+++ voxelmorph/__init__.py
@@ -22,43 +22,14 @@ from . import generators
 from . import py
 from .py.utils import default_unet_features
 
+try:
+    import torch
+except ImportError:
+    raise ImportError('Please install pytorch to use this voxelmorph backend')
 
-# import backend-dependent submodules
-backend = py.utils.get_backend()
+os.environ['NEURITE_BACKEND'] = 'pytorch'
 
-if backend == 'pytorch':
-    # the pytorch backend can be enabled by setting the VXM_BACKEND
-    # environment var to "pytorch"
-    try:
-        import torch
-    except ImportError:
-        raise ImportError('Please install pytorch to use this voxelmorph backend')
-
-    os.environ['NEURITE_BACKEND'] = 'pytorch'
-
-    from . import torch
-    from .torch import layers
-    from .torch import networks
-    from .torch import losses
-
-else:
-    # tensorflow is default backend
-    try:
-        import tensorflow
-    except ImportError:
-        raise ImportError('Please install tensorflow to use this voxelmorph backend')
-
-    os.environ['NEURITE_BACKEND'] = 'tensorflow'
-
-    # ensure valid tensorflow version is available
-    minv = '2.4'
-    curv = getattr(tensorflow, '__version__', None)
-    if curv is None or version.parse(curv) < version.parse(minv):
-        raise ImportError(f'voxelmorph requires tensorflow version {minv} or greater, '
-                          f'but found version {curv}')
-
-    from . import tf
-    from .tf import layers
-    from .tf import networks
-    from .tf import losses
-    from .tf import utils
+from . import torch
+from .torch import layers
+from .torch import networks
+from .torch import losses
