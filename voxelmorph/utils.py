b3ce5ff4320be1f505d4f77efc7ca82f0fe52a6b
Etienne Chollet <etiennepchollet@gmail.com>
2025-07-17 17:42:22 -0400

    [feat] add integrate_displacement_field() and supporting functions from voxynth

--- voxelmorph/utils.py
+++ voxelmorph/utils.py
@@ -7,6 +7,10 @@ from torch import Tensor
 
 __all__ = [
     "affine_to_displacement_field",
+    "grid_coordinates",
+    "spatial_transform",
+    "displacement_field_to_coords",
+    "integrate_displacement_field"
 ]
 
 
@@ -57,3 +61,122 @@ def affine_to_displacement_field(
     shift = shift.view(*shape, ndim) - grid
 
     return shift
+
+
+def grid_coordinates(shape, device: torch.device = None) -> Tensor:
+    """
+    TODOC
+    """
+    ranges = [torch.arange(s, dtype=torch.float32, device=device) for s in shape]
+    meshgrid = torch.stack(torch.meshgrid(*ranges, indexing='ij'), dim=-1)
+    return meshgrid
+
+
+def spatial_transform(
+    image: Tensor,
+    trf: Tensor,
+    method: str = 'linear',
+    isdisp: bool = True,
+    meshgrid: Tensor = None,
+    rotate_around_center: bool = True
+) -> Tensor:
+    """
+    TODOC
+    """
+    if trf is None:
+        return image
+
+    if trf.ndim == 2:
+        if meshgrid is None:
+            meshgrid = grid_coordinates(image.shape[1:], device=image.device)
+
+        trf = torch.linalg.inv(trf)
+        trf = affine_to_displacement_field(
+            trf,
+            meshgrid,
+            rotate_around_center=rotate_around_center
+        )
+        isdisp = True
+
+    if isdisp:
+        # convert the displacement crs to absolute crs scaled to range [-1, 1]
+        trf = displacement_field_to_coords(trf, meshgrid=meshgrid)
+
+    method = 'bilinear' if method == 'linear' else method
+
+    reset_type = None
+    if not torch.is_floating_point(image):
+        if method == 'nearest':
+            reset_type = image.dtype
+        image = image.type(torch.float32)
+
+    image = image.unsqueeze(0)
+    trf = trf.unsqueeze(0)
+
+    # trf is an absolute crs field in the range of [-1, 1]
+    interped = torch.nn.functional.grid_sample(image, trf, align_corners=True, mode=method)
+    interped = interped.squeeze(0)
+
+    if reset_type is not None:
+        interped = interped.type(reset_type)
+
+    return interped
+
+
+def displacement_field_to_coords(disp, meshgrid=None) -> Tensor:
+    """
+    Convert the displacement crs to absolute crs scaled to range [-1, 1].
+
+    Parameters:
+    -----------
+    disp: torch.Tensor
+        Displacement crs field
+    meshgrid: torch.Tensor, optional
+       crs grid for the image shape
+
+    Returns:
+    --------
+    torch.Tensor:
+        The absolute crs field scaled to range [-1, 1].
+    """
+    if meshgrid is None:
+        meshgrid = grid_coordinates(disp.shape[:-1], device=disp.device)
+
+    shape = disp.shape[:-1]
+    ndim = disp.shape[-1]
+
+    # compute the absolute crs field
+    # scale the field to range [-1, 1], which is expected by torch.nn.functional.grid_sample()
+    coords = (meshgrid + disp)
+    for d in range(ndim):
+        if shape[d] == 1:
+            coords[..., d] *= 0
+        else:
+            coords[..., d] *= 2 / (shape[d] - 1)
+            coords[..., d] -= 1
+
+    coords = coords.flip(-1)
+
+    return coords
+
+
+def integrate_displacement_field(
+    disp: Tensor,
+    steps: int,
+    meshgrid: Tensor = None
+) -> Tensor:
+    """
+    TODOC
+    """
+    if meshgrid is None:
+        # generate a crs grid
+        meshgrid = grid_coordinates(disp.shape[:-1], device=disp.device)
+
+    if steps == 0:
+        return disp
+
+    disp = disp / (2 ** steps)
+    for _ in range(steps):
+        disp += spatial_transform(disp.movedim(-1, 0), disp, meshgrid=meshgrid).movedim(0, -1)
+
+    return disp
