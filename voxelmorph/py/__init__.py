62a486cd22f9cb3cdcb95336339e89a306f71fae
Etienne Chollet <etiennepchollet@gmail.com>
2025-09-02 11:18:05 -0400

    [feat] add module table to py init

--- voxelmorph/py/__init__.py
+++ voxelmorph/py/__init__.py
@@ -1,5 +1,12 @@
 """
 The `py/` submodule contains python (non-pytorch) implementations of various helper functions.
+
+Modules
+-------
+generators
+    Data generators for medical image registration.
+utils
+    General-purpuse python utilities for VoxelMorph.
 """
 
 from . import utils
