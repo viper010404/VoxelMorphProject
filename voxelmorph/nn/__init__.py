e9848a1a4a7686da648ae139bb8446bdae3da26d
Etienne Chollet <etiennepchollet@gmail.com>
2025-09-02 11:06:55 -0400

    [refactor] add modules table to docstring and add an __all__

--- voxelmorph/nn/__init__.py
+++ voxelmorph/nn/__init__.py
@@ -1,8 +1,29 @@
 """
-Torch-based neural network layers and functions for VoxelMorph.
+Torch-based neural network components for VoxelMorph. This subpackage contains the functional
+operators, reusable building blocks, model definitions, and loss functions to implement the
+VoxelMorph framework in PyTorch.
+
+Modules
+-------
+functional
+    Functions containing the core operations and logic of for image registration written in
+    PyTorch.
+losses
+    Loss functions for image registration.
+models
+    Core VoxelMorph models for unsupervised and supervised learning.
+modules
+    Neural network building blocks for VoxelMorph.
 """
 
 from . import functional
 from . import losses
 from . import models
 from . import modules
+
+__all__ = [
+    "functional",
+    "losses",
+    "models",
+    "modules",
+]
