e826aeb02ba456ec833eefae673bdcf495db6557
Etienne Chollet <etiennepchollet@gmail.com>
2025-12-11 19:27:49 -0500

    [refactor] Deprecate NCC and Grad with NotImplementedError

--- voxelmorph/nn/losses.py
+++ voxelmorph/nn/losses.py
@@ -17,61 +17,14 @@ class NCC:
     """
 
     def __init__(self, win=None):
-        self.win = win
+        raise NotImplementedError(
+            'voxelmorph.nn.losses.NCC is deprecated. Use neurite.nn.modules.NCC instead.'
+        )
 
     def loss(self, y_true, y_pred):
-
-        Ii = y_true
-        Ji = y_pred
-
-        # get dimension of volume
-        # assumes Ii, Ji are sized [batch_size, *vol_shape, nb_feats]
-        ndims = len(list(Ii.size())) - 2
-        assert ndims in [1, 2, 3], "volumes should be 1 to 3 dimensions. found: %d" % ndims
-
-        # set window size
-        win = [9] * ndims if self.win is None else self.win
-
-        # compute filters - use the same device as input tensors
-        sum_filt = torch.ones([1, 1, *win], device=Ii.device, dtype=Ii.dtype)
-
-        pad_no = math.floor(win[0] / 2)
-
-        if ndims == 1:
-            stride = (1)
-            padding = (pad_no)
-        elif ndims == 2:
-            stride = (1, 1)
-            padding = (pad_no, pad_no)
-        else:
-            stride = (1, 1, 1)
-            padding = (pad_no, pad_no, pad_no)
-
-        # get convolution function
-        conv_fn = getattr(F, 'conv%dd' % ndims)
-
-        # compute CC squares
-        I2 = Ii * Ii
-        J2 = Ji * Ji
-        IJ = Ii * Ji
-
-        I_sum = conv_fn(Ii, sum_filt, stride=stride, padding=padding)
-        J_sum = conv_fn(Ji, sum_filt, stride=stride, padding=padding)
-        I2_sum = conv_fn(I2, sum_filt, stride=stride, padding=padding)
-        J2_sum = conv_fn(J2, sum_filt, stride=stride, padding=padding)
-        IJ_sum = conv_fn(IJ, sum_filt, stride=stride, padding=padding)
-
-        win_size = np.prod(win)
-        u_I = I_sum / win_size
-        u_J = J_sum / win_size
-
-        cross = IJ_sum - u_J * I_sum - u_I * J_sum + u_I * u_J * win_size
-        I_var = I2_sum - 2 * u_I * I_sum + u_I * u_I * win_size
-        J_var = J2_sum - 2 * u_J * J_sum + u_J * u_J * win_size
-
-        cc = cross * cross / (I_var * J_var + 1e-5)
-
-        return -torch.mean(cc)
+        raise NotImplementedError(
+            'voxelmorph.nn.losses.NCC is deprecated. Use neurite.nn.modules.NCC instead.'
+        )
 
 
 class MSE:
@@ -80,12 +33,12 @@ class MSE:
     """
 
     def __init__(self, *args, **kwargs):
-        raise DeprecationWarning(
+        raise NotImplementedError(
             "voxelmorph.nn.losses.MSE is deprecated. Use neurite.nn.modules.MSE instead."
         )
 
     def loss(self, y_true, y_pred):
-        raise DeprecationWarning(
+        raise NotImplementedError(
             "voxelmorph.nn.losses.MSE is deprecated. Use neurite.nn.modules.MSE instead."
         )
 
@@ -96,12 +49,12 @@ class Dice:
     """
 
     def __init__(self, *args, **kwargs):
-        raise DeprecationWarning(
+        raise NotImplementedError(
             "voxelmorph.nn.losses.Dice is deprecated. Use neurite.nn.modules.Dice instead."
         )
 
     def loss(self, y_true, y_pred):
-        raise DeprecationWarning(
+        raise NotImplementedError(
             "voxelmorph.nn.losses.Dice is deprecated. Use neurite.nn.modules.Dice instead."
         )
 
@@ -112,65 +65,16 @@ class Grad:
     """
 
     def __init__(self, penalty='l1', loss_mult=None):
-        self.penalty = penalty
-        self.loss_mult = loss_mult
+        raise NotImplementedError(
+            "voxelmorph.nn.losses.Grad is deprecated. Use neurite.nn.modules.Grad instead."
+        )
 
     def _diffs(self, y):
-        vol_shape = [n for n in y.shape][2:]
-        ndims = len(vol_shape)
-
-        df = [None] * ndims
-        for i in range(ndims):
-            d = i + 2
-            # permute dimensions
-            r = [d, *range(0, d), *range(d + 1, ndims + 2)]
-            y = y.permute(r)
-            dfi = y[1:, ...] - y[:-1, ...]
-
-            # permute back
-            # note: this might not be necessary for this loss specifically,
-            # since the results are just summed over anyway.
-            r = [*range(d - 1, d + 1), *reversed(range(1, d - 1)), 0, *range(d + 1, ndims + 2)]
-            df[i] = dfi.permute(r)
-
-        return df
+        raise NotImplementedError(
+            "voxelmorph.nn.losses.Grad is deprecated. Use neurite.nn.modules.Grad instead."
+        )
 
     def loss(self, y_pred):
-        """
-        Compute gradient penalty loss.
-
-        Computes spatial gradients using finite differences along each dimension,
-        applies L1 or L2 penalty, and returns the average across all dimensions.
-
-        Parameters
-        ----------
-        _ : None
-            Placeholder for target (not used in gradient loss).
-        y_pred : torch.Tensor
-            Predicted displacement or velocity field with shape (B, ndim, *spatial_dims).
-
-        Returns
-        -------
-        torch.Tensor
-            Gradient penalty loss with shape (B,) containing per-batch loss values.
-
-        Notes
-        -----
-        - Expects channels-first format: (B, ndim, *spatial_dims)
-        - Do NOT pass PyTorch grids in (B, *spatial_dims, ndim) format
-        - Computes mean gradient magnitude across all spatial locations and channels
-        """
-    def loss(self, y_pred):
-        if self.penalty == 'l1':
-            dif = [torch.abs(f) for f in self._diffs(y_pred)]
-        else:
-            assert self.penalty == 'l2', 'penalty can only be l1 or l2. Got: %s' % self.penalty
-            dif = [f * f for f in self._diffs(y_pred)]
-
-        df = [torch.mean(torch.flatten(f, start_dim=1), dim=-1) for f in dif]
-        grad = sum(df) / len(df)
-
-        if self.loss_mult is not None:
-            grad *= self.loss_mult
-
-        return grad.mean()
+        raise NotImplementedError(
+            "voxelmorph.nn.losses.Grad is deprecated. Use neurite.nn.modules.Grad instead."
+        )
